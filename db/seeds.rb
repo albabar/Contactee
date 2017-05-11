84e15ad9dad5420ad2db9fd22c816630e41005fd
Babar Al-Amin <knock@babar.im>
2017-05-12 03:26:19 +0600

    Seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,24 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Seeding Users'
+User.create! first_name: 'Babar', last_name: 'Al-Amin', email: 'knock@babar.im', password: 'asdasd123', password_confirmation: 'asdasd123'
+User.create! first_name: 'Another', last_name: 'User', email: 'user@example.com', password: 'asdasd123', password_confirmation: 'asdasd123'
+
+puts 'Seeding groups'
+Group.create! name: 'Friends', user: User.first
+Group.create! name: 'Family', user: User.first
+Group.create! name: 'Work', user: User.first
+Group.create! name: 'FnF', user: User.second
+Group.create! name: 'Team Mates', user: User.second
+Group.create! name: 'Family', user: User.second
+
+puts 'Seeding Contacts'
+Contact.create! first_name: 'Elon', last_name: 'Musk', email: 'em@example.com', user: User.first, group_ids: User.first.group_ids.sample(rand(0..3))
+Contact.create! first_name: 'Linus', last_name: 'Torvalds', email: 'lt@example.com', user: User.first, group_ids: User.first.group_ids.sample(rand(0..3))
+Contact.create! first_name: 'David', last_name: 'Heinemeier Hansson', email: 'dhh@example.com', user: User.first, group_ids: User.first.group_ids.sample(rand(0..3))
+Contact.create! first_name: 'Jason', last_name: 'Fried', email: 'jf@example.com', user: User.first, group_ids: User.first.group_ids.sample(rand(0..3)), organization: 'Basecamp', is_organization: true
+Contact.create! first_name: 'Elon', last_name: 'Musk', email: 'em@example.com', user: User.second, group_ids: User.first.group_ids.sample(rand(0..3))
+
+puts '=' * 20
+puts "You can now login with email: #{User.pluck(:email).join(' / ')} and password: asdasd123"
+puts '=' * 20
